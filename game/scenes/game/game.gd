f0af45bd339c98c773cebe1f5a38826b5b855d1d
yaseen-ahmed26 <yaseen.ahmed26@outlook.com>
2026-07-10 20:23:44 +0100

    feat: click effect when clicking the button

--- game/scenes/game/game.gd
+++ game/scenes/game/game.gd
@@ -8,6 +8,26 @@ func _ready() -> void:
 
 func _on_clicker_btn_pressed() -> void:
 	PlayerManager.click_cookie()
-
+	
+	var clone: RichTextLabel = $effect.duplicate()
+	
+	self.add_child(clone)
+	var starting_position = Vector2(randf_range(100, 1700), 1080)
+	var end_position = starting_position - Vector2(0, randf_range(200, 1000))
+	
+	clone.position = starting_position
+	
+	var tween: Tween = create_tween()
+	tween.tween_property(clone, "position", end_position, 0.4)
+	
+	await get_tree().create_timer(0.5).timeout
+	
+	var tween_fade: Tween = create_tween()
+	tween_fade.tween_property(clone, "modulate:a", 0.0, 0.5)
+	
+	await tween_fade.finished
+	clone.queue_free()
+	
 func _on_stats_changed(stats):
 	cookie_counter.text = "COOKIES: %.1f" % stats.biscuits
+	$effect.text = "+%d" % stats.per_click
