44360003962c3f5a5180cde18789fff3243cd42a
yaseen-ahmed26 <yaseen.ahmed26@outlook.com>
2026-07-11 20:19:54 +0100

    fix: fix multiple bugs relating to upgrade lines

--- game/scenes/upgrades/upgrade_line/upgrade_line.gd
+++ game/scenes/upgrades/upgrade_line/upgrade_line.gd
@@ -6,6 +6,94 @@ extends Control
 @onready var description: RichTextLabel = $description
 @onready var levels: VBoxContainer = $levels
 
+var red_panel: StyleBoxFlat = preload("res://resources/panels/red.tres")
+var green_panel: StyleBoxFlat = preload("res://resources/panels/green.tres")
+var gold_panel: StyleBoxFlat = preload("res://resources/panels/gold.tres")
+
+var upgrade_data: Dictionary
+var current_level: int = 0
+
+func _get_current_level_data():
+	return upgrade_data.levels[current_level]
+
+func _change_panel_colour(panel_name, colour):
+	var panel: Panel = levels.get_node(panel_name)
+	panel.add_theme_stylebox_override("panel", colour)
+
+func _apply_effect():
+	var level_info: Dictionary = _get_current_level_data()
+	
+	_change_panel_colour(str(current_level), green_panel)
+	
+	current_level += 1
+	PlayerManager.apply_effect(level_info.effect)
+	PlayerManager.bought_upgrade(self.get_meta("id"), current_level)
+
+func _update_ui():
+	if current_level >= upgrade_data.total_levels:
+		_change_panel_colour(str(current_level - 1), gold_panel)
+		
+		$info_panel/level_name.visible = false
+		$info_panel/level_description.visible = false
+		$info_panel/level_cost.visible = false
+		
+		$buy_btn.disabled = true
+		$buy_btn.text = "COMPLETE"
+		
+		return
+	
+	var level_info: Dictionary = _get_current_level_data()
+	
+	$info_panel/level_name.text = level_info.name
+	$info_panel/level_description.text = level_info.description
+	$info_panel/level_cost.text = "%d Biscuits" % level_info.cost
+
+func reset_line():
+	set_up_line(upgrade_data, 0)
+	$buy_btn.text = "BUY"
+	$buy_btn.disabled = false
+	
+	$info_panel/level_name.visible = true
+	$info_panel/level_description.visible = true
+	$info_panel/level_cost.visible = true
+
+func set_up_line(data, saved_level):
+	current_level = 0
+	
+	upgrade_data = data
+
+	upgrade_name.text = data.name
+	description.text = data.description
+			
+	self.set_meta("id", data.id)
+	self.set_meta("group", data.group)
+	self.name = data.id
+
+	for panel in levels.get_children():
+		panel.add_theme_stylebox_override("panel", red_panel)
+	
+	for i in range(0, saved_level):
+		_apply_effect()
+	
+	_update_ui()
+
+func _on_buy_btn_pressed():
+	var level_info: Dictionary = _get_current_level_data()
+	
+	if PlayerManager.can_purchase(level_info.cost):	
+		_apply_effect()
+		_update_ui()
+	else:
+		print("not enough")
+
+"""extends Control
+
+@onready var icon: TextureRect = $icon
+@onready var info_panel: Panel = $info_panel
+@onready var upgrade_name: RichTextLabel = $upgrade_name
+@onready var description: RichTextLabel = $description
+@onready var levels: VBoxContainer = $levels
+
 var upgrade_data: Dictionary
 var current_level: int = 0
 
@@ -19,6 +107,14 @@ func _get_current_level_data():
 func _set_all_panels_red():
 	for panel in levels.get_children():
 		panel.add_theme_stylebox_override("panel", red_panel)
+
+func _buy_level():
+	var level_info: Dictionary = _get_current_level_data()
+	
+	current_level += 1
+	PlayerManager.apply_effect(level_info.effect)
+	PlayerManager.bought_upgrade(self.get_meta("id"), current_level)
+	_display_level()
 
 func _display_level():
 	if current_level >= upgrade_data.total_levels:
@@ -40,7 +136,6 @@ func _display_level():
 	panel.add_theme_stylebox_override("panel", green_panel)
 
 func set_up_line(data: Dictionary, saved_level: int):
-	print("Loading save data. Current saved level is ", saved_level)
 	upgrade_data = data
 
 	upgrade_name.text = data.name
@@ -51,10 +146,13 @@ func set_up_line(data: Dictionary, saved_level: int):
 	self.name = data.id
 	
 	_set_all_panels_red()
-
-	for i in range(0, saved_level + 1):
-		current_level = i
+	
+	if saved_level == 0:
 		_display_level()
+		return
+	
+	for i in range(0, saved_level):
+		_buy_level()
 
 func reset_line():
 	set_up_line(upgrade_data, 0)
@@ -65,9 +163,7 @@ func _on_buy_btn_pressed():
 	var level_info: Dictionary = _get_current_level_data()
 	
 	if PlayerManager.can_purchase(level_info.cost):	
-		current_level += 1
-		PlayerManager.apply_effect(level_info.effect)
-		PlayerManager.bought_upgrade(self.get_meta("id"), current_level)
-		_display_level()
+		_buy_level()
 	else:
 		print("not enough")
+"""
